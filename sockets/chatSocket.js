7e285d52252dfd667b9df1e3f7144a1a9413d493
Sachin Rathore <sachin.rathore0110@gmail.com>
2017-10-25 21:00:33 +0530

    Add socket events for chat

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,28 +1,58 @@
-
 const ChatSocketHelper = {
   users: [],
 
   onConnection(socketIO) {
     socketIO.on('connection', (socket) => {
       console.log('connection establishment is in progress...');
-      
-      socket.on('join', (socketData) => { 
-        socket.join(socketData);
-        const {id} = socket;
 
-        this.users.push({id, userName: socketData});
+      socket.on('join', (socketData) => {
+        socket.join(socketData);
+        const {
+          id
+        } = socket;
+
+        this.users.push({
+          id,
+          nickname: socketData,
+          online: true,
+        });
         let sockets = [];
 
-        Object.keys(socketIO.sockets.sockets).forEach((userId) => {
-          sockets = this.users.filter((user) => user.id === userId)      
-        })
+        Object.keys(socketIO.sockets.sockets).forEach((id) => {
+          for (const user of this.users) {
+            if (user.id == id) {
+              sockets.push(user);
+            }
+          }
+        });
 
         socketIO.sockets.emit('users', sockets)
-      })
+      });
 
       socket.on('send', (socketData) => {
         socketIO.sockets.to(socketData.receiver).emit('receiver', socketData)
-      })
+      });
+
+      socket.on('typing', (socketData) => {
+        console.log('typing', socketData)
+        socketIO.sockets.to(socketData.chatId).emit('typing', socketData)
+      });
+      
+      socket.on('stop-typing', (socketData) => {
+        console.log('stop-typing', socketData)
+        socketIO.sockets.to(socketData.chatId).emit('stop-typing', socketData)
+      });
+
+      socket.on('disconnect', (socketData) => {
+        this.users = this.users.map(user => {
+          if (user.id === socket.id) {
+            console.log('disconnected', user)
+            user.online = false;
+          }
+
+          return user;
+        })
+      });
     })
   }
 
